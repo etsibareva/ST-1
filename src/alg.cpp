3b778604a1843d800e4cc1a8fa27751bbf983778
etsibareva <etsibareva.university@gmail.com>
2026-02-26 11:44:35 +0300

    Commit0: Init

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,23 +2,46 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2) return false;
+  if (value == 2) return true;
+  if (value % 2 == 0) return false;
+  
+  for (uint64_t i = 3; i * i <= value; i += 2) {
+      if (value % i == 0) return false;
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n == 0) return 0;
+  
+  uint64_t count = 0;
+  uint64_t num = 2;
+  
+  while (count < n) {
+    if (checkPrime(num)) {
+        count++;
+    }
+    num++;
+  }
+  return num - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t result = value + 1;
+  while (!checkPrime(result)) {
+      result++;
+  }
+  return result;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  for (uint64_t num = 2; num < hbound; num++) {
+      if (checkPrime(num)) {
+          sum += num;
+      }
+  }
+  return sum;
 }
